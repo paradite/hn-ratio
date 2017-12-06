88edda1706210ab9c9bca935f8a42017a8cf7400
Zhu Liang <zhuliangg11@gmail.com>
2017-12-07 01:06:28 +0800

    MISC: visual fixes

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-d3.json('results/2017-dec-01.json', function(data) {
-  console.log(data);
+const date = '2017-dec-03';
+
+d3.json(`results/${date}.json`, function(data) {
   
-  var dataContent = d3.select('.content')
-    .selectAll('div')
+  var contentWrapper = d3.select('.content');
+
+  contentWrapper.append('div')
+    .classed('info', true)
+    .text('[Ratio] Title (Comment/Score)')
+
+  contentWrapper.append('div')
+    .classed('date-picker', true)
+    .text(`Select a date: ${date}`)
+
+  var dataContent = contentWrapper
+    .selectAll('div.row')
     .data(data);
 
   var contentEnterWrapper = dataContent
     .enter()
-    .append('div');
+    .append('div')
+    .classed('row', true);
 
   contentEnterWrapper
     .append('a')
@@ -16,6 +28,15 @@ d3.json('results/2017-dec-01.json', function(data) {
       return d.hn_url;
     })
     .text(function(d) {
-      return `${d.ratio.toFixed(2)} ${d.title} (${d.score}/${d.comments})`
+      return `[${d.ratio.toFixed(2)}] ${d.title} (${d.comments}/${d.score})`;
+    })
+
+  contentWrapper.append('p')
+    .append('a')
+    .attr('href', function(d) {
+      return 'https://github.com/paradite/hn-ratio';
+    })
+    .text(function(d) {
+      return 'GitHub';
     })
 });
