4993865eb68360acc4fcc0671c7b00623299fcae
Zhu Liang <zhuliangg11@gmail.com>
2017-12-16 17:25:13 +0800

    MISC: check for file existence, remove today's data for CI test

--- hn-ratio.py
+++ hn-ratio.py
@@ -3,6 +3,7 @@ from hackernews import HackerNews
 import json
 import datetime
 import os.path
+from pathlib import Path
 hn = HackerNews()
 
 ITEM_LIMIT = 50
@@ -23,7 +24,12 @@ def story_serializer(story):
     }
     return story_attrs
 
-if __name__ == "__main__":
+def execute(date):
+    fn_json = os.path.join(os.path.dirname(__file__), "results", "{0}.json".format(date).lower())
+    json_file = Path(fn_json)
+    if json_file.is_file():
+        print("Data already exist for today, exiting.")
+        return
     stories = []
     for story_id in hn.top_stories(limit=ITEM_LIMIT):
         story = hn.get_item(story_id)
@@ -38,30 +44,9 @@ if __name__ == "__main__":
         stories.append(story)
         story.hn_url = "https://news.ycombinator.com/item?id=" + str(story.item_id)
     stories.sort(key=lambda s:s.ratio, reverse=True)
-    
-    current_date = datetime.datetime.now().strftime("%Y-%b-%d")
-    fn_json = os.path.join(os.path.dirname(__file__), "results", "{0}.json".format(current_date).lower())
-    fn_txt = os.path.join(os.path.dirname(__file__), "results", "{0}-visual.txt".format(current_date).lower())
     with open(fn_json, "w+") as f:
         f.write(json.dumps([story_serializer(story) for story in stories], indent=4, sort_keys=True))
-    with open(fn_txt, "w+") as f:
-        for story in stories:
-            f.write(story.title + "\n")
-            f.write("https://news.ycombinator.com/item?id=" + str(story.item_id) + "\n")
-            
-            f.write("|")
-            if story.blocks <= WIDTH:
-                for i in range(story.blocks):
-                    f.write("#")
-                for i in range(WIDTH - story.blocks):
-                    f.write(" ")
-                f.write("|")
-            else:
-                for i in range(WIDTH):
-                    f.write("#")
-                f.write("|")
-                for i in range(story.blocks - WIDTH):
-                    f.write("#")
 
-            f.write(" S: {0} C: {1} R: {2:.3f}\n\n".format(
-                story.score, story.descendants, story.ratio))
+if __name__ == "__main__":
+    current_date = datetime.datetime.now().strftime("%Y-%b-%d")
+    execute(current_date)
